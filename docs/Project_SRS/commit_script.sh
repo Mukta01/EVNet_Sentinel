08afbdfc53e5968c94324262adbe82cffc5906da
Shardul Chogale <144150911+shard-c6@users.noreply.github.com>
2026-09-08 22:15:26 +0530

    feat: add project planning documentation and automate Random Forest training notebook generation

---
+++ docs/Project_SRS/commit_script.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+git checkout -b docs/project-planning
+git add docs/Project_SRS/EVNet_Sentinel_Project_Planning.tex
+git commit -F docs/Project_SRS/commit_msg.txt
+rm docs/Project_SRS/commit_msg.txt
